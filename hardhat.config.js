eac6dc2e7732effcef7360f55610b82ea86c46a4
Morkus Salasevicius <salaseviciusmorkus@gmail.com>
2022-02-19 14:42:43 +0000

    Configure hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require('@nomiclabs/hardhat-web3');
+const { API_URL, PRIVATE_KEY } = process.env;
+module.exports = {
+   solidity: "0.8.1",
+   defaultNetwork: "rinkeby",
+   networks: {
+      hardhat: {
+      },
+      rinkeby: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
